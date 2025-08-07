0189f5466596a4d65005cf383b3f2984a8121bda
Devang Awasthi <devangawasthi82@gmail.com>
2025-08-07 23:45:07 +0530

    added health point

--- src/main/java/com/chicken/ChickenApplication.java
+++ src/main/java/com/chicken/ChickenApplication.java
@@ -3,10 +3,22 @@ package com.chicken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class ChickenApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ChickenApplication.class, args);
-	}
 
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ChickenApplication.class);
+		Map<String, Object> props = new HashMap<>();
+		String port = System.getenv("PORT");
+
+		if (port != null) {
+			props.put("server.port", port);
+		}
+
+		app.setDefaultProperties(props);
+		app.run(args);
+	}
 }
